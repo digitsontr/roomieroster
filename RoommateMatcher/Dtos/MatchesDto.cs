a2a7cbd48990ff568b3c9b73c3ffcc9350f91e01
digitsontr <admin@digitson.com.tr>
2024-02-15 09:07:06 +0000

    Update Artifacts

--- RoommateMatcher/Dtos/MatchesDto.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace RoommateMatcher.Dtos
-{
-	public class MatchesDto
-	{
-		public List<MatchDto> Matches { get; set; }
-	}
-}
-
--- RoommateMatcher/Models/Chat.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RoommateMatcher.Models
-{
-	public class Chat
-	{
-		public int Id { get; set; }
-		public List<Message> Messages { get; set; }
-	}
-}
-
