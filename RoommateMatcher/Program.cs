7e744e2c96111e4cf584049d13c9f78d6e5fffba
admin <admin@roomieroster.com.tr>
2024-03-26 22:39:44 +0300

    hosting changes

--- RoommateMatcher/Program.cs
+++ RoommateMatcher/Program.cs
@@ -112,11 +112,10 @@ builder.Services.AddAuthentication(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles(new StaticFileOptions
